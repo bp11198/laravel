3321b0a4d3565c3c958b08f1c1dad018f24ac1c6
phuongbinh <bp11198>
2025-03-09 19:03:29 +0700

    add stress cpu

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cpu-stress', function () {
+// Tăng CPU bằng cách tính toán một vòng lặp lớn
+    $result = 0;
+    for ($i = 0; $i < 10000000000; $i++) {
+        $result += sqrt($i); // Tính căn bậc hai liên tục để tiêu tốn CPU
+    }
+    return "CPU stress completed. Result: $result";
+});
